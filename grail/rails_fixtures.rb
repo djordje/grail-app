ff9c3bd1fa5bc4708fb2dd8ef10512d6af32679b
Djordje Kovacevic <djordje.bigjoe@gmail.com>
2018-04-24 23:34:18 +0200

    Fix rails fixtures

--- grail/rails_fixtures.rb
+++ grail/rails_fixtures.rb
@@ -7,22 +7,23 @@ unless defined? Rails
   # Unless Rails exists mock namespace and necessary methods
   #   This makes ActiveRecord rake tasks functional
   module Rails
+    extend SingleForwardable
     # :nodoc:
     class Paths
       def paths
         {
           'db/migrate' => [
-            Grail::App.instance.db_migrate_dir
+            Grail.app.db_migrate_dir
           ]
         }
       end
 
       def load_seed
-        load Grail::App.instance.db_dir.join('seeds.rb')
+        load Grail.app.db_dir.join('seeds.rb')
       end
     end
 
-    delegate :root, :env, to: 'Grail::App.instance'
+    delegate %i[root env] => 'Grail.app'
 
     def self.application
       Paths.new
